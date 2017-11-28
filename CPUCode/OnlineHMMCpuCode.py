0297b7e1b9927fd31140fce53ea84d9eb4535775
bkrosenz <bkrosenz@indiana.edu>
2017-11-28 13:13:44 -0500

    python

--- CPUCode/OnlineHMMCpuCode.py
+++ CPUCode/OnlineHMMCpuCode.py
@@ -2,42 +2,86 @@ from sys import argv
 import argparse, os
 import numpy as np
 
-class learner():
+STEP = 200
+MAX = 50000
+
+def file_reader(fn):
+    with open(fn,'r') as f:
+        for n,line in enumerate(f):
+            if n>MAX:
+                raise StopIteration
+            else:
+                yield int(line)
+
+class Learner():
     def __init__(self,N,M,eta=0.01):
-        self.eta = 0.01
+        self.eta = eta
         self.N = N
         self.M = M
-        self.phi = np.ndarray([N,N,M,N])
-        self.gamma = np.ndarray([N,N,M])
-        self.q = np.ndarray([N])
-        self.a = np.ndarray([N,N])
-        self.b = np.ndarray([N,M])
-
+        init = 0.001
+        self.phi = np.reshape(
+            np.repeat(init,N**3*M),(N,N,M,N)
+        )
+        alpha = 5
+        self.gamma = np.random.dirichlet([alpha]*M, (N,N))
+        self.q = np.random.dirichlet([alpha]*N, 1)[0]
+        self.a = np.random.dirichlet([alpha]*N, N)
+        self.b = np.random.dirichlet([alpha]*M, N)
+        print self.phi.shape
+        print 'a',self.a.shape,'q',self.q.shape,'b',self.b[:,0]
+        
     def initialize(self):
-        self.a = 1./self.N
-        self.b = 1./self.M
-
-    def update(y):
+        # self.a /= self.N
+        # self.b /= self.M
+        # self.q /= self.N
+        
+        self.n = 0
+        
+    def update_phi(self,y):
         for i in range(self.N):
             for j in range(self.N):
                 for k in range(self.M):
+                    match_ky = k==y
                     for h in range(self.N):
-                        self.phi[i,j,k,h] = sum( self.gamma[l,h,y] * (phi[i,j,k,l] + eta * g * match * q[l] - phi[i,j,k,l]) for l in range(self.N) )
+                        match_jh = j==h
+                        self.phi[i,j,k,h] = sum(
+                            self.gamma[l,h,y] * (self.phi[i,j,k,l] +
+                                                 self.eta * (
+                                                     match_ky * match_jh * (i==l) * self.q[l] - self.phi[i,j,k,l]
+                                                 )
+                            )
+                            for l in range(self.N)
+                        )
+                            
+                        
+    def update(self,y):
+            
+        if not self.n%STEP:
+            print 'gam',self.gamma, 'y',y,'n',self.n,'q',self.q,'a',self.a,'b',self.b
+            print self.phi
 
-        gamma_denom = np.einsum()
-        np.dot(q,np.dot(a,b))
-        for l in range(self.N):
-            for h in range(self.N):
-                gamma[l,h,y] = a[l,h]*b[h,y] / gamma_denom
+        self.update_phi(y)
+        
+        self.n+=1
 
-        q = np.einsum('m,ml->m',q,gamma)
+        if self.n<100:
+            return
+        
+        gamma_denom = np.einsum('m,mn,n->',self.q,self.a,self.b[:,y])
+        self.gamma[:,:,y] = np.einsum('lh,h->lh',self.a,self.b[:,y]) / gamma_denom
+        
+        self.q = np.einsum('m,ml->m',self.q,self.gamma[:,:,y])
+        a_denom = 1. / np.einsum('ijkh->i',self.phi)
+        self.a = np.einsum('ijkh,i->ij',self.phi,a_denom) 
 
-        self.a = np.einsum('ijkh->ij',self.phi)
+        b_denom = 1. / np.einsum('ijkh->j',self.phi)
+        self.b = np.einsum('ijkh,j->jk',self.phi,b_denom) 
 
-        a_denom = np.einsum('ijkh->i',self.phi)
+def main(args):
+    hmm = Learner(2,4)
+    hmm.initialize()
+    for y in file_reader(args[0]):
+        hmm.update(y)    
 
-        self.a
-
-        self.b = np.einsum('ijkh->jk',self.phi)
-
-        b_denom = np.einsum('ijkh->j',self.phi)
+if __name__ == "__main__":
+    main(argv[1:])
